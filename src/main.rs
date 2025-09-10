50a3378a8afd229fe3d26c6979672aa11844ac3c
DontStarve72 <helinos67@protonmail.com>
2025-09-10 01:25:23 -0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+use bevy::{
+    prelude::*,
+    window::{CursorGrabMode, PrimaryWindow},
+};
+use bevy_framepace::{FramepacePlugin, FramepaceSettings, Limiter};
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
+use bevy_rapier3d::prelude::*;
+use bevy_trenchbroom::{
+    TrenchBroomPlugins,
+    config::TrenchBroomConfig,
+    prelude::{ComputeLightmapSettings, SpawnHooks},
+};
+use nil::ShortToString;
+
+use crate::player::{
+    CameraSettings, ControllerAimAcceleration, JumpSettings, PlayerController, PlayerPlugin,
+};
+
+mod player;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(AmbientLight::NONE)
+        //.insert_resource(Time::<Fixed>::from_hz(60.0))
+        .add_plugins((
+            DefaultPlugins,
+            RapierPhysicsPlugin::<()>::default(),
+            RapierDebugRenderPlugin::default(),
+            bevy_panic_handler::PanicHandler::new().build(),
+            PlayerPlugin,
+            TrenchBroomPlugins(
+                TrenchBroomConfig::new("Carrot")
+                    .default_solid_spawn_hooks(|| SpawnHooks::new().convex_collider())
+                    .bicubic_lightmap_filtering(true)
+                    .compute_lightmap_settings(ComputeLightmapSettings {
+                        extrusion: 1,
+                        ..default()
+                    })
+                    .texture_extensions(vec![
+                        "bmp".s(),
+                        "exr".s(),
+                        "hdr".s(),
+                        "jpeg".s(),
+                        "jpg".s(),
+                        "png".s(),
+                        "tga".s(),
+                        "webp".s(),
+                        "D".s(),
+                        "C".s(),
+                    ]),
+            ),
+            EguiPlugin::default(),
+            WorldInspectorPlugin::new(),
+            FramepacePlugin,
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, grab_cursor)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    //mut meshes: ResMut<Assets<Mesh>>,
+    //mut materials: ResMut<Assets<StandardMaterial>>,
+    mut framepace_setting: ResMut<FramepaceSettings>,
+    asset_server: Res<AssetServer>,
+) {
+    framepace_setting.limiter = Limiter::Auto;
+
+    //let material = materials.add(Color::WHITE);
+    //let material_2 = materials.add(Color::srgb(1.0, 0.0, 0.0));
+
+    commands.spawn(SceneRoot(asset_server.load("maps/test.bsp#Scene")));
+
+    commands.spawn((
+        Collider::cuboid(10.0, 1.0, 10.0),
+        Transform::from_xyz(0.0, -0.5, 0.0),
+    ));
+
+    commands.spawn((
+        PlayerController,
+        ControllerAimAcceleration::default(),
+        JumpSettings::default(),
+    ));
+}
+
+fn grab_cursor(
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+    mut camera_settings: Single<&mut CameraSettings>,
+    button: Res<ButtonInput<MouseButton>>,
+    key: Res<ButtonInput<KeyCode>>,
+) -> Result {
+    let mut window = windows.single_mut()?;
+
+    if key.just_pressed(KeyCode::KeyF) && window.cursor_options.grab_mode == CursorGrabMode::Locked
+    {
+        window.cursor_options.grab_mode = CursorGrabMode::Confined;
+        window.cursor_options.visible = true;
+        camera_settings.allow_movement = false;
+    }
+
+    if key.just_released(KeyCode::KeyF)
+        && window.cursor_options.grab_mode == CursorGrabMode::Confined
+    {
+        window.cursor_options.grab_mode = CursorGrabMode::Locked;
+        window.cursor_options.visible = false;
+        camera_settings.allow_movement = true;
+    }
+
+    if button.just_pressed(MouseButton::Left)
+        && window.cursor_options.grab_mode == CursorGrabMode::None
+    {
+        window.cursor_options.grab_mode = CursorGrabMode::Locked;
+        window.cursor_options.visible = false;
+        camera_settings.allow_movement = true;
+    }
+
+    if key.just_pressed(KeyCode::Escape)
+        && window.cursor_options.grab_mode == CursorGrabMode::Locked
+    {
+        window.cursor_options.grab_mode = CursorGrabMode::None;
+        window.cursor_options.visible = true;
+        camera_settings.allow_movement = false;
+    }
+
+    Ok(())
+}
---
+++ src/player.rs
@@ -0,0 +1,408 @@
+use std::f32::consts::FRAC_PI_2;
+
+use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*, scene::SceneInstanceReady};
+use bevy_rapier3d::prelude::*;
+use bevy_trenchbroom::class::builtin::InfoPlayerStart;
+use nestify::nest;
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<MovementAction>()
+            .init_resource::<FOV>()
+            .add_systems(PreUpdate, (mouse_input, keyboard_input))
+            .add_systems(Update, (headbob_effect, look, movement.after(look)))
+            .add_observer(spawn_player);
+    }
+}
+
+#[derive(Event)]
+pub enum MovementAction {
+    Move {
+        input_direction: Vec2,
+        movement_speed: f32,
+    },
+    Jump,
+    Look {
+        delta: Vec2,
+    },
+}
+
+#[derive(Resource)]
+pub enum FOV {
+    horizontal_degrees_4x3(f32),
+    horizontal_degrees_16x9(f32),
+    horizontal_degrees_aspect_ratio(f32),
+    vertical_degrees(f32),
+    horizontal_radians_4x3(f32),
+    horizontal_radians_16x9(f32),
+    horizontal_radians_aspect_ratio(f32),
+    vertical_radians(f32),
+}
+
+impl Default for FOV {
+    fn default() -> Self {
+        Self::horizontal_degrees_aspect_ratio(90.0)
+    }
+}
+
+impl Into<f32> for &FOV {
+    fn into(self) -> f32 {
+        match self {
+            FOV::horizontal_degrees_4x3(h_deg_4x3) => todo!(),
+            FOV::horizontal_degrees_16x9(h_deg_16x9) => todo!(),
+            FOV::horizontal_degrees_aspect_ratio(h_deg_ratio) => h_deg_ratio.to_radians(),
+            FOV::vertical_degrees(v_deg) => todo!(),
+            FOV::horizontal_radians_4x3(h_rad_4x3) => todo!(),
+            FOV::horizontal_radians_16x9(h_rad_16x9) => todo!(),
+            FOV::horizontal_radians_aspect_ratio(h_rad_ratio) => *h_rad_ratio,
+            FOV::vertical_radians(v_rad) => todo!(),
+        }
+    }
+}
+
+nest! {
+    #[derive(Component)]
+    pub struct CameraSettings {
+        pub allow_movement: bool,
+
+        #>[derive(Clone, Copy)]
+        pub mouse_sensitivity: pub enum CameraSensitivity {
+            Linear(f32),
+            Relative(Vec2),
+        },
+        pub controller_sensitivity: CameraSensitivity,
+    }
+}
+
+impl Into<Vec2> for CameraSensitivity {
+    fn into(self) -> Vec2 {
+        match self {
+            CameraSensitivity::Linear(linear) => Vec2::splat(linear),
+            CameraSensitivity::Relative(relative) => relative,
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct JumpSettings {
+    impulse: f32,
+    auto_bhop: bool,
+}
+
+impl Default for JumpSettings {
+    fn default() -> Self {
+        Self {
+            impulse: 6.0,
+            auto_bhop: false,
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct MovementSettings {
+    walking_speed: f32,
+    sprinting_speed: Option<f32>,
+    crouching_speed: Option<f32>,
+
+    ground_acceleration: f32,
+    ground_deceleration: f32,
+    ground_friction: f32,
+
+    air_cap: f32,
+    air_acceleration: f32,
+    air_move_speed: f32,
+}
+
+#[derive(Component)]
+#[require(HeadbobTime)]
+pub struct HeadbobSettings {
+    headbob_move_amount: f32,
+    headbob_frquency: f32,
+}
+
+impl Default for HeadbobSettings {
+    fn default() -> Self {
+        Self {
+            headbob_move_amount: 0.06,
+            headbob_frquency: 2.4,
+        }
+    }
+}
+
+#[derive(Component, Default)]
+struct HeadbobTime(f32);
+
+#[derive(Component)]
+pub struct ControllerAimAcceleration(f32);
+
+impl Default for ControllerAimAcceleration {
+    fn default() -> Self {
+        Self(5.0)
+    }
+}
+
+#[derive(Component, Default)]
+struct PlayerVelocity(Vec3);
+
+#[derive(Component, Default)]
+#[require(
+    Transform,
+    CameraSettings {
+        allow_movement: false,
+        mouse_sensitivity: CameraSensitivity::Linear(0.003),
+        controller_sensitivity: CameraSensitivity::Linear(0.005),
+    },
+    MovementSettings {
+        walking_speed: 7.0,
+        sprinting_speed: Some(8.5),
+        crouching_speed: Some(3.5),
+
+        ground_acceleration: 14.0,
+        ground_deceleration: 10.0,
+        ground_friction: 6.0,
+
+        air_cap: 0.05,
+        air_acceleration: 800.0,
+        air_move_speed: 500.0,
+    },
+    RigidBody::KinematicPositionBased,
+    PlayerVelocity,
+    KinematicCharacterController {
+        max_slope_climb_angle: 45_f32.to_radians(),
+        autostep: Some(CharacterAutostep {
+            max_height: CharacterLength::Absolute(0.3),
+            min_width: CharacterLength::Absolute(0.25),
+            include_dynamic_bodies: true,
+        }),
+        custom_shape: Some((Collider::cuboid(0.25, 1.0, 0.25), Vec3::new(0.0, 1.0, 0.0), Quat::default())),
+        ..default()
+    },
+    GravityScale(1.5),
+)]
+
+pub struct PlayerController;
+
+#[derive(Component)]
+struct WorldModelCameraMarker;
+
+fn spawn_player(
+    _trigger: Trigger<SceneInstanceReady>,
+    player_entity_query: Single<(Entity, &mut Transform), With<PlayerController>>,
+    player_start_query: Populated<&Transform, (With<InfoPlayerStart>, Without<PlayerController>)>,
+    fov: Res<FOV>,
+    mut commands: Commands,
+) {
+    let (entity, mut player_transform) = player_entity_query.into_inner();
+
+    let transforms: Vec<&Transform> = player_start_query.iter().collect();
+    if let Some(player_start_transform) = transforms.get(0) {
+        *player_transform = **player_start_transform;
+    }
+
+    commands.entity(entity).with_children(|parent| {
+        parent.spawn((
+            WorldModelCameraMarker,
+            Camera3d::default(),
+            Projection::from(PerspectiveProjection {
+                fov: fov.into_inner().into(),
+                ..default()
+            }),
+            Transform::from_xyz(0.0, 1.79, 0.0),
+        ));
+    });
+}
+
+fn keyboard_input(
+    player_settings: Single<(Option<&JumpSettings>, &MovementSettings), With<PlayerController>>,
+    mut movement_event_writer: EventWriter<MovementAction>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let (jump_settings, movement_settings) = player_settings.into_inner();
+
+    let forward = keyboard_input.pressed(KeyCode::KeyW);
+    let backward = keyboard_input.pressed(KeyCode::KeyS);
+    let left = keyboard_input.pressed(KeyCode::KeyA);
+    let right = keyboard_input.pressed(KeyCode::KeyD);
+
+    let lateral = right as i8 - left as i8;
+    let longitudinal = backward as i8 - forward as i8;
+    let input_direction = Vec2::new(lateral as f32, longitudinal as f32).normalize_or_zero();
+
+    if input_direction != Vec2::ZERO {
+        movement_event_writer.write(MovementAction::Move {
+            input_direction,
+            movement_speed: if let Some(sprinting_speed) = movement_settings.sprinting_speed
+                && keyboard_input.pressed(KeyCode::ShiftLeft)
+            {
+                sprinting_speed
+            } else {
+                movement_settings.walking_speed
+            },
+        });
+    }
+
+    if let Some(jump_settings) = jump_settings {
+        if keyboard_input.just_pressed(KeyCode::Space)
+            || (jump_settings.auto_bhop && keyboard_input.pressed(KeyCode::Space))
+        {
+            movement_event_writer.write(MovementAction::Jump);
+        }
+    }
+}
+
+fn mouse_input(
+    camera_settings: Single<&CameraSettings>,
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    mut movement_event_writer: EventWriter<MovementAction>,
+) {
+    let delta = accumulated_mouse_motion.delta;
+    if delta != Vec2::ZERO && camera_settings.allow_movement {
+        let sensitivity: Vec2 = camera_settings.mouse_sensitivity.into();
+        let delta = -delta * sensitivity;
+
+        movement_event_writer.write(MovementAction::Look { delta });
+    }
+}
+
+fn look(
+    player_query: Single<&mut Transform, (With<PlayerController>, Without<ChildOf>)>,
+    camera_query: Single<&mut Transform, (With<Camera3d>, With<ChildOf>)>,
+    mut movement_event_reader: EventReader<MovementAction>,
+) {
+    let mut player_transform = player_query.into_inner();
+    let mut camera_transform = camera_query.into_inner();
+
+    for event in movement_event_reader.read() {
+        match event {
+            MovementAction::Look { delta } => {
+                let (_, camera_pitch, _) = camera_transform.rotation.to_euler(EulerRot::YXZ);
+
+                const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+                let clamped_delta =
+                    (camera_pitch + delta.y).clamp(-PITCH_LIMIT, PITCH_LIMIT) - camera_pitch;
+
+                player_transform.rotate_local_y(delta.x);
+                camera_transform.rotate_local_x(clamped_delta);
+            }
+            _ => (),
+        }
+    }
+}
+
+fn movement(
+    time: Res<Time>,
+    mut movement_event_reader: EventReader<MovementAction>,
+    query: Single<
+        (
+            &Transform,
+            &mut PlayerVelocity,
+            &mut KinematicCharacterController,
+            Option<&KinematicCharacterControllerOutput>,
+            Option<&JumpSettings>,
+            &MovementSettings,
+            &GravityScale,
+        ),
+        With<PlayerController>,
+    >,
+) {
+    let delta_time = time.delta_secs();
+    let (
+        transform,
+        mut velocity,
+        mut controller,
+        output,
+        jump_settings,
+        movement_settings,
+        gravity_scale,
+    ) = query.into_inner();
+
+    let vertical_velocity = &mut velocity.0.y;
+    let is_grounded = output.map_or(false, |o| o.grounded && *vertical_velocity <= 0.0);
+
+    // Ground movement
+    if is_grounded {
+        *vertical_velocity = 0.0;
+
+        for event in movement_event_reader.read() {
+            match event {
+                MovementAction::Move {
+                    input_direction,
+                    movement_speed,
+                } => {
+                    let wish_direction = transform
+                        .rotation
+                        .mul_vec3(input_direction.extend(0.0).xzy());
+                    let current_speed_in_wish_direction = velocity.0.dot(wish_direction);
+                    let add_speed_till_cap = movement_speed - current_speed_in_wish_direction;
+                    if add_speed_till_cap > 0.0 {
+                        let mut acceleration =
+                            movement_settings.ground_acceleration * movement_speed * delta_time;
+                        acceleration = acceleration.min(add_speed_till_cap);
+
+                        velocity.0 += acceleration * wish_direction;
+                    }
+                }
+                MovementAction::Jump => {
+                    if let Some(jump_settings) = jump_settings {
+                        velocity.0.y = jump_settings.impulse;
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        let lateral_velocity = velocity.0.xz().length();
+        let deceleration_force = lateral_velocity.max(movement_settings.ground_deceleration);
+        let deceleration_velocity =
+            deceleration_force * movement_settings.ground_friction * delta_time as f32;
+        let mut new_speed = (lateral_velocity - deceleration_velocity).max(0.0);
+        if lateral_velocity > 0.0 {
+            new_speed /= lateral_velocity;
+        }
+
+        velocity.0.x *= new_speed;
+        velocity.0.z *= new_speed;
+    }
+    // Air movement
+    else {
+        *vertical_velocity -= 9.8 * gravity_scale.0 * delta_time;
+
+        for event in movement_event_reader.read() {
+            match event {
+                MovementAction::Move {
+                    input_direction,
+                    movement_speed: _,
+                } => {
+                    let wish_direction = transform
+                        .rotation
+                        .mul_vec3(input_direction.extend(0.0).xzy());
+                    let current_speed_in_wish_direction = velocity.0.dot(wish_direction);
+                    let capped_speed = (movement_settings.air_move_speed * wish_direction)
+                        .length()
+                        .min(movement_settings.air_cap);
+                    let add_speed_till_cap = capped_speed - current_speed_in_wish_direction;
+                    if add_speed_till_cap > 0.0 {
+                        let mut acceleration_speed = movement_settings.air_acceleration
+                            * movement_settings.air_move_speed
+                            * delta_time;
+                        acceleration_speed = acceleration_speed.min(add_speed_till_cap);
+
+                        velocity.0 += acceleration_speed * wish_direction;
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    controller.translation = Some(velocity.0 * delta_time);
+}
+
+fn headbob_effect(
+    time: Res<Time>,
+    headbob_settings: Option<Single<&HeadbobSettings, With<PlayerController>>>,
+) {
+    if let Some(headbob_settings) = headbob_settings.map(|hs| hs.into_inner()) {}
+}
